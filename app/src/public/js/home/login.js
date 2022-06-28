6e32ca8a9391455d6a90f2d3e6fdbe3e8a70284b
Wocgc <choigyucheol@naver.com>
2022-06-28 11:53:32 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+// querySelector 직역하면 질의 선택자이고 파라미터에서 받아오는 선택자를 통해서 html의 값을 가져올 수 있음
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+//로그인 버튼을 눌렀을 때 id값을 가져오는 함수 선언
+//요청하는 데이터니까 req변수에 obj로 담아줌
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+}
