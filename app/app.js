53ac8aa617b7ad382d52097231dcfb6f6e3a72f2
Wocgc <choigyucheol@naver.com>
2022-06-28 10:54:37 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//express 라는 프레임워크 사용
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const home = require("./src/routes/home");
+
+//app 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home);
+//home을 저장하고 만들어진 index.js 파일의 경로를 명시해줌
+
+module.exports = app;
+
+//git bash에서 node 실행 후 app.js 파일을 js로 옮겨줌
+// -s는 --save랑 똑같음
+//npm app.js로 서버 가동 후 localhost:포트번호로 서버 창 확인
---
+++ app/src/routes/home/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//app이 아닌 router로 선언하고 app을 router로 치환
+const express = require("express");
+const router = express.Router();
+
+//ctrl안에 있는 hello와 login 불러와 연결함
+const ctrl = require("./home.ctrl");
+
+router.get("/", ctrl.hello);
+router.get("/login", ctrl.login);
+
+module.exports = router;
+
+//index.js를 app.js로 연결
+//라우터를 외부파일에서 사용 가능하게 연결해줌, 내보냄
