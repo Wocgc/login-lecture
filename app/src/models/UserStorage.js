0cc4711c48dd58a2bb84653d0eb3b4b2a527f5ed
Wocgc <choigyucheol@naver.com>
2022-06-30 11:11:30 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,21 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
-  //외부에서 접근하지 못하게 숨기는 것을 은닉화라고 하는데 # 로 처리하면 된다
-  static #users = {
-    id: ["gyucheol", "나개발", "김팀장"],
-    psword: ["1234", "1234", "123456"],
-    name: ["최규철", "나개발", "김팀장"],
-  };
-  //은닉화를 하게 되면 데이터를 받아올 수가 없는데 그것을 받아 올 수 있게 메소드로 처리하고 전달헤야함
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
+  }
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -21,17 +27,16 @@ class UserStorage {
     return newUsers;
   }
   static getUserInfo(id) {
-    const users = this.#users; // users를 받아옴
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // users의 key값들만 리스트로 만듦 =>[id, psword, name]
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data);
+      })
+      .catch(console.error);
   }
+
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
